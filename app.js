f7eb3b84e97c08e0dc74b71f1662a094c19b5099
mgcodeur <mgcodeur@gmail.com>
2023-01-23 10:42:05 +0300

    dynamic port

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ const app = express();
 
 app.use(process.env.BASE_PATH, createProxyMiddleware({ target: process.env.BASE_URL, changeOrigin: true }));
 
-app.listen(3000);
+app.listen(process.env.APP_PORT || 3000);
